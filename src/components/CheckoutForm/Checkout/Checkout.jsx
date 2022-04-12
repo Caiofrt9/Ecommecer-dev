14d8936928a6792d3568e735c940719d472a7e1f
Caio F <silvacaio295@gmail.com>
2022-04-12 14:18:04 -0300

    correcting errors

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,79 +1,95 @@
-import React,{useState, useEffect } from 'react'
-import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button, CssBaseline} from '@material-ui/core'
+import React, { useState, useEffect } from 'react';
+import { CssBaseline, Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
+import { Link, useHistory } from 'react-router-dom';
 
-import { useHistory } from 'react-router-dom'
+import { commerce } from '../../../lib/commerce';
+import AddressForm from '../AdressForm';
+import PaymentForm from '../PaymentForm';
+import useStyles from './styles';
 
-import { commerce } from '../../../lib/commerce'
-import useStyles from './styles'
-import AddressForm from '../AdressForm'
-import PaymentForm from '../PaymentForm'
+const steps = ['Shipping address', 'Payment details'];
 
-const steps = ['Shipping adress', 'Payment details']
+const Checkout = ({ cart, onCaptureCheckout, order, error }) => {
+  const [checkoutToken, setCheckoutToken] = useState(null);
+  const [activeStep, setActiveStep] = useState(0);
+  const [shippingData, setShippingData] = useState({});
+  const classes = useStyles();
+  const history = useHistory();
 
-const Checkout = ({ cart, order, onCaptureCheckout, error }) => {
-
-  const [activeStep, setActiveStep] = useState(0)
-  const [checkoutToken, setCheckoutToken] = useState(null)
-  const [shippingData, setShippingData] = useState({})
-  const classes = useStyles()
-  const history = useHistory
+  const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1);
 
   useEffect(() => {
-    const generateToken = async () => {
-      try{
-        const token = await commerce.checkout.generateToken(cart.id, {type:'cart'})
+    if (cart.id) {
+      const generateToken = async () => {
+        try {
+          const token = await commerce.checkout.generateToken(cart.id, { type: 'cart' });
 
-        setCheckoutToken(token)
-      } catch (error) {
-        history.pushState()
+          setCheckoutToken(token);
+        } catch {
+          if (activeStep !== steps.length) history.push('/');
+        }
+      };
 
-      }
+      generateToken();
     }
+  }, [cart]);
 
-    generateToken()
-  },[cart])
+  const test = (data) => {
+    setShippingData(data);
 
-  const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1 )
-  const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1 )
+    nextStep();
+  };
 
-  const next = (data) => {
-    setShippingData(data)
+  let Confirmation = () => (order.customer ? (
+    <>
+      <div>
+        <Typography variant="h5">Thank you for your purchase, {order.customer.firstname} {order.customer.lastname}!</Typography>
+        <Divider className={classes.divider} />
+        <Typography variant="subtitle2">Order ref: {order.customer_reference}</Typography>
+      </div>
+      <br />
+      <Button component={Link} variant="outlined" type="button" to="/">Back to home</Button>
+    </>
+  ) : (
+    <div className={classes.spinner}>
+      <CircularProgress />
+    </div>
+  ));
 
-    nextStep()
+  if (error) {
+    Confirmation = () => (
+      <>
+        <Typography variant="h5">Error: {error}</Typography>
+        <br />
+        <Button component={Link} variant="outlined" type="button" to="/">Back to home</Button>
+      </>
+    );
   }
 
-  const Confirmation = () => (
-    <div>
-      Confirmation
-    </div>
-  )
+  const Form = () => (activeStep === 0
+    ? <AddressForm checkoutToken={checkoutToken} nextStep={nextStep} setShippingData={setShippingData} test={test} />
+    : <PaymentForm checkoutToken={checkoutToken} nextStep={nextStep} backStep={backStep} shippingData={shippingData} onCaptureCheckout={onCaptureCheckout} />);
 
-  const Form = () => activeStep === 0 
-    ? <AddressForm checkoutToken={checkoutToken} next={next}/>
-    : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} nextStep={nextStep} backStep={backStep} onCaptureCheckout={onCaptureCheckout}/>
   return (
     <>
-
-    <CssBaseline/>
-
-      <div className={classes.toolbar}/>
-      <main classeName={classes.layout}>
-        <Paper className={classes.paper} sm={3}>
-          <Typography variant ='h5' align='center'>Checkout</Typography>
+      <CssBaseline />
+      <div className={classes.toolbar} />
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Typography variant="h4" align="center">Checkout</Typography>
           <Stepper activeStep={activeStep} className={classes.stepper}>
-            {steps.map((step) => (
-              <Step key={step}>
-                <StepLabel>{step}</StepLabel>
+            {steps.map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
               </Step>
             ))}
           </Stepper>
-
-            {activeStep === steps.lenght ? <Confirmation/> : checkoutToken && <Form/>}
-
+          {activeStep === steps.length ? <Confirmation /> : checkoutToken && <Form />}
         </Paper>
       </main>
     </>
-    )
-}
+  );
+};
 
-export default Checkout
+export default Checkout;
--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,48 +1,43 @@
-import React from 'react'
-import { Typography, Button, Divider } from '@material-ui/core' 
-import { Elements, CardElement, ElementsConsumer} from '@stripe/react-stripe-js'
-import { loadStripe } from '@stripe/stripe-js'
+import React from 'react';
+import { Typography, Button, Divider } from '@material-ui/core';
+import { Elements, CardElement, ElementsConsumer } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
-import  Review from './Review'
+import Review from './Review';
 
 const stripePromise = loadStripe(`${process.env.REACP_APP_STRIPE_PUBLIC_KEY}`)
 
 const PaymentForm = ({ checkoutToken, nextStep, backStep, shippingData, onCaptureCheckout }) => {
-
   const handleSubmit = async (event, elements, stripe) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    if(!stripe || !elements) return 
+    if (!stripe || !elements) return;
 
-    const  cardElement = elements.getElement(CardElement)
+    const cardElement = elements.getElement(CardElement);
 
     const { error, paymentMethod } = await stripe.createPaymentMethod({ type: 'card', card: cardElement });
 
-    if(error) {
-      console.log(error)
-    }else {
+    if (error) {
+      console.log('[error]', error);
+    } else {
       const orderData = {
         line_items: checkoutToken.live.line_items,
-        custumer: { firstname: shippingData.firstname, lastname: shippingData.lastname, email: shippingData.email},
-        shipping: {
-          name: 'Primary',
-          street: shippingData.adress1, town_city:shippingData.city,
-          town_city: shippingData.city,
-          county_state: shippingData.shippingSubdivision,
-          postal_zip_code: shippingData.zip,
-          country: shippingData.shippingCountry
-        },
-        fullfillment: {shipping_method: shippingData.shippingOption},
+        customer: { firstname: shippingData.firstName, lastname: shippingData.lastName, email: shippingData.email },
+        shipping: { name: 'International', street: shippingData.address1, town_city: shippingData.city, county_state: shippingData.shippingSubdivision, postal_zip_code: shippingData.zip, country: shippingData.shippingCountry },
+        fulfillment: { shipping_method: shippingData.shippingOption },
         payment: {
           gateway: 'stripe',
           stripe: {
-            payment_method_id: paymentMethod.id
-          }
-        }
-      }
-    }
-  }
+            payment_method_id: paymentMethod.id,
+          },
+        },
+      };
 
+      onCaptureCheckout(checkoutToken.id, orderData);
+
+      nextStep();
+    }
+  };
 
   return (
     <>
@@ -65,7 +60,7 @@ const PaymentForm = ({ checkoutToken, nextStep, backStep, shippingData, onCaptur
         </ElementsConsumer>
       </Elements>
     </>
-  )
-}
+  );
+};
 
-export default PaymentForm
+export default PaymentForm;
